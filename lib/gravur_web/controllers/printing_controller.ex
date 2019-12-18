9b2a361827e6a6db4a08b06bdc110a15e4177b32
Arkadiusz Plichta <el.pikel@gmail.com>
2019-12-18 19:21:47 +0100

    pdf generation

--- lib/gravur_web/controllers/printing_controller.ex
+++ lib/gravur_web/controllers/printing_controller.ex
@@ -2,11 +2,20 @@ defmodule GravurWeb.PrintingController do
   use GravurWeb, :controller
 
   def create(conn, %{"book_id" => book_id}) do
-    html = "<html><body><p>Hi there!</p></body></html>"
-    # be aware, this may take a while...
+    html = Gravur.Core.get_book_with_greetings(book_id)
+      |> generate_html
+
     {:ok, filename}    = PdfGenerator.generate(html, page_size: "A5")
     {:ok, pdf_content} = File.read(filename)
-    File.write("/Users/arkadiuszplichta/Repositories/test.pdf", pdf_content)
+
+    # upload to aws and update book
+
+    # bucket_name = "S3_BUCKET"
+    # s3_path = "uploads"
+    # filename
+    #   |> ExAws.S3.Upload.stream_file
+    #   |> ExAws.S3.upload(bucket_name, s3_path)
+    #   |> ExAws.request!
 
     conn
       |> put_resp_content_type("application/pdf")
@@ -14,5 +23,22 @@ defmodule GravurWeb.PrintingController do
         "content-disposition",
         "attachment; filename=\"#{filename}\"")
       |> send_resp(200, pdf_content)
+  end
+
+  defp generate_html(book) do
+    greetings = book.greetings
+     |> Enum.map(fn greeting -> "<li><img width=\"500\" src=\"#{get_image_url(greeting)}\" /><div>#{greeting.text}</div><div>#{greeting.signature}</div></li>" end)
+
+    "<html><body><p><ul style=\"list-style-type:none;\">#{greetings}</ul</p></body></html>"
+  end
+
+  defp get_image_url(greeting) do
+    url = Gravur.Utils.Image.url({ greeting.image, greeting })
+
+    if String.starts_with?(url, "https:") do
+      url
+    else
+      "http://localhost:4000" <> url
+    end
   end
 end
