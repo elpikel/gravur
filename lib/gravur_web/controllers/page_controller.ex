db3bb3fb39557e3f48dca8b2469b2a869b35f002
Arkadiusz Plichta <el.pikel@gmail.com>
2020-02-12 19:11:46 +0100

    landing page

--- lib/gravur_web/controllers/page_controller.ex
+++ lib/gravur_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule GravurWeb.PageController do
   use GravurWeb, :controller
 
+  plug :put_layout, "landing.html"
+
   def index(conn, _params) do
     if Gravur.Identity.user_signed_in?(conn) do
       redirect(conn, to: GravurWeb.Router.Helpers.book_path(GravurWeb.Endpoint, :index))
