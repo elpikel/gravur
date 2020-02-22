9eba64db25755d673baebb3a5fe14f7b6202b931
Arkadiusz Plichta <el.pikel@gmail.com>
2020-02-22 16:04:56 +0100

    fixing

--- lib/gravur/printing/printing.ex
+++ lib/gravur/printing/printing.ex
@@ -5,6 +5,7 @@ defmodule Gravur.Printing do
     html = generate_html(book)
 
     IO.inspect(html)
+
     {:ok, filename} = PdfGenerator.generate(html, page_size: "A5")
     {:ok, content} = File.read(filename)
     {:ok, url} = Gravur.Utils.BookPdf.store({ filename, book })
@@ -16,10 +17,21 @@ defmodule Gravur.Printing do
     url = Gravur.Utils.GreetingImage.url({ greeting.image, greeting })
 
     if String.starts_with?(url, "https:") do
-      url
+      String.replace(url, "https", "http")
     else
       "http://localhost:4000" <> url
     end
+  end
+
+  def get_image_base64(greeting) do
+    url = get_image_url(greeting)
+    IO.inspect url
+    Application.ensure_all_started :inets
+
+    {:ok, resp} = :httpc.request(:get, {to_charlist(url), []}, [], [body_format: :binary])
+    {{_, 200, 'OK'}, _headers, body} = resp
+
+    Base.url_encode64(body)
   end
 
   defp generate_html(book) do
