c23b9f7e3f3f96994a4a871d0fa0e08d063dc4b4
Arkadiusz Plichta <el.pikel@gmail.com>
2020-02-22 15:06:13 +0100

    pdf tests

--- lib/gravur/printing/printing.ex
+++ lib/gravur/printing/printing.ex
@@ -4,7 +4,8 @@ defmodule Gravur.Printing do
   def generate_pdf(book) do
     html = generate_html(book)
 
-    {:ok, filename} = PdfGenerator.generate(html, page_size: "A5")
+    # {:ok, filename} = PdfGenerator.generate(html, page_size: "A5")
+    {:ok, filename} = PdfGenerator.generate {:url, "http://google.com"}, page_size: "A5"
     {:ok, content} = File.read(filename)
     {:ok, url} = Gravur.Utils.BookPdf.store({ filename, book })
 
@@ -14,12 +15,13 @@ defmodule Gravur.Printing do
   def get_image_url(greeting) do
     url = Gravur.Utils.GreetingImage.url({ greeting.image, greeting })
 
-    IO.inspect(url)
     if String.starts_with?(url, "https:") do
       url
     else
       "http://localhost:4000" <> url
     end
+
+    "https://gravur.s3.amazonaws.com/uploads/book/0c997136-ac1f-4cdd-ba2d-deaaf25bfd19/greetings/fLEACvsQiQ-xB2NS6psGjakub-sejkora-42069-unsplash.jpg?v=63749523978"
   end
 
   defp generate_html(book) do
