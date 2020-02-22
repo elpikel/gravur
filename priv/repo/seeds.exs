10a85793c3b9cc633009ee608565f35296527ada
Arkadiusz Plichta <el.pikel@gmail.com>
2020-02-22 16:20:12 +0100

    new templates

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,5 +10,6 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-Gravur.Repo.insert!(%Gravur.Core.Template{name: "wzór drzewny", image: "/images/one.jpg"})
-Gravur.Repo.insert!(%Gravur.Core.Template{name: "wzór kwiatowy", image: "/images/two.jpg"})
+#Gravur.Repo.insert!(%Gravur.Core.Template{name: "wzór drzewny", image: "/images/one.jpg"})
+#Gravur.Repo.insert!(%Gravur.Core.Template{name: "wzór kwiatowy", image: "/images/two.jpg"})
+Gravur.Repo.insert!(%Gravur.Core.Template{name: "wzór minimalistyczny", image: "/images/three.jpg"})
