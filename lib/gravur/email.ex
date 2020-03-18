96e40f3dec649c12b1a0dba9b4b0786e5f3c38f8
Arkadiusz Plichta <el.pikel@gmail.com>
2020-03-18 22:31:34 +0100

    mail fix

--- lib/gravur/email.ex
+++ lib/gravur/email.ex
@@ -4,10 +4,11 @@ defmodule Gravur.Email do
   def verify_email(conn, user) do
     IO.puts("emai: #{user.email}")
 
-    new_email()
-    |> from("gravur@gravur.com")
-    |> to(user.email)
-    |> subject("Welcome!")
+    new_email(
+      to: user.email,
+      from: "gravur@gravur.com",
+      subject: "Welcome"
+    )
     |> render("verify.html", conn: conn, user: user)
     |> Gravur.Externals.Mailer.deliver_later()
   end
