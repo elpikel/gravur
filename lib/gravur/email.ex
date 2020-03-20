2575b59ab36692ae9f928e64e1c59f8658e395a1
Arkadiusz Plichta <el.pikel@gmail.com>
2020-03-20 11:37:03 +0100

    email configuration

--- lib/gravur/email.ex
+++ lib/gravur/email.ex
@@ -4,9 +4,8 @@ defmodule Gravur.Email do
   def verify_email(conn, user) do
     new_email(
       to: user.email,
-      from: "gravur@gravur.com",
-      subject: "Welcome",
-      text_body: "test"
+      from: "mailgun@sandbox8105b95b353e4017977fadc6f864f2e3.mailgun.org",
+      subject: "Welcome"
     )
     |> render("verify.html", conn: conn, user: user)
     |> Gravur.Externals.Mailer.deliver_later()
